e73b8fddddd83c4b06cd77e58474857e02b86972
Brayan Lozada <brayanlozada1102@gmail.com>
2026-03-26 10:57:50 -0500

    advance 1

---
+++ core.py
@@ -0,0 +1,41 @@
+def validate_inputs(type : type, message:str, is_number:bool= False) -> str | float | int :
+    """
+    Validate the inputs to ensure they are in the correct format
+    If it's a number, set the last argument to true.
+    """
+    warning = ""
+    while message:
+        try:
+            a = type(input(f"Enter {message}{warning}: "))
+            if is_number:
+                if a <= 0:
+                    print("Enter a positive number")
+                    warning = " positive"
+                    continue
+            return a
+        except:
+            print(f"Enter a valid {'number' if is_number else 'letter'}")
+            warning = " again"
+            
+def inventory_register(product_name: str,quantity: int,unit_value: float,daily_sales: list):
+    update = {
+            "name":product_name,
+            "unitvalue" : unit_value,
+            "quantity" : quantity,
+         
+         }
+    daily_sales.append(update)
+    print(f"Product {product_name} successfully registered!")
+
+def show_summary(daily_sales: list) -> list :
+    for i in daily_sales:
+        print(f"{i['name']} \nQuantity of product in the inventory: {i['quantity']} \nPrice per unit: ${i['unitvalue']}\nTotal value of the product in inventory: ${i['unitvalue']*i['quantity']}")
+        print("--------------------")
+
+
+inventario = None 
+if inventario == False:
+    print("vacio")
+else:
+    print ("llenito")
+    print (inventario)
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+from core import validate_inputs, inventory_register
 
+inventorylist = []
+def main(customers_db, products_db, orders_db, order_id):
+    
+    #This is the introduction message
+    print("--- Inventory System ---")
+
+    #We run the program In a while loop, so the program only stop when we decide.  
+    option = ":)"
+    while option != 0 :
+        #This is the menu options.
+        print("\n1. Register Product \n2. View Inventory \n3. Calculate Statistics \n4. View Orders\n5. Daily Income\n6. Final Report & Exit")
+        option = input("Select an option: ")
+        
+        if option == "1":
+            #The first option is to create a new user in the user database.
+            product_name_validated = validate_inputs(str, "name product")
+            price_validated = validate_inputs(float,"price",True)
+            quantity_validated = validate_inputs(int,"quantity",True)
+            
+            inventory_register(product_name_validated,quantity_validated,price_validated,inventorylist)
+          
+            print("Customer registered.")
+            
+        elif option == "2":
+            #This is the option to create a new product in the product database.
+            
+            
+            print("Product registered.")
